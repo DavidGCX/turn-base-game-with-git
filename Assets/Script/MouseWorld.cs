53169741e48e59f6ecd203b155d3e84329b46f5a
DGCX <c62gao@uwaterloo.ca>
2022-11-30 14:38:21 -0500

    fix the mouse world plane

--- Assets/Script/MouseWorld.cs
+++ Assets/Script/MouseWorld.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MouseWorld : MonoBehaviour
 {
     [field: SerializeField] public Transform sphere {get; private set;}
+    [SerializeField] private LayerMask mousePlaneLayerMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,7 @@ public class MouseWorld : MonoBehaviour
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out RaycastHit rayCastHit)) {
-         
+        if(Physics.Raycast(ray, out RaycastHit rayCastHit, float.MaxValue, mousePlaneLayerMask)) {
             Debug.Log("hit");
             sphere.position = rayCastHit.point;
         }
