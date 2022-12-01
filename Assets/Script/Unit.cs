9c2e7004a8d71bf19577baec3567a31be4eaa083
DGCX <c62gao@uwaterloo.ca>
2022-12-01 11:28:23 -0500

    some animation up

--- Assets/Script/Unit.cs
+++ Assets/Script/Unit.cs
@@ -8,7 +8,7 @@ public class Unit : MonoBehaviour
 {
     [SerializeField] private Animator animator; 
     private Vector3 targetPosition;
-    private const float stopDistance = 0.1f;
+    private const float stopDistance = 0.3f;
     [SerializeField] private float moveSpeed;
     // Start is called before the first frame update
     void Start()
@@ -20,12 +20,14 @@ public class Unit : MonoBehaviour
     {
         Vector3 moveDirection = (targetPosition - transform.position).normalized;
         if(Vector3.Distance(transform.position, targetPosition) > stopDistance) {
-            transform.LookAt(targetPosition);
-            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+            //transform.position += moveDirection * Time.deltaTime * moveSpeed;
+            animator.SetFloat("IdleToRun", 10, 0.1f, Time.deltaTime);
+        } else {
+            animator.SetFloat("IdleToRun", 0, 0.1f, Time.deltaTime);
         }
         if (Input.GetMouseButtonDown(0)) {
-
             Move(MouseWorld.GetMousePosition());
+            transform.LookAt(targetPosition);
         }
     }
     private void Move(Vector3 targetPosition) {
