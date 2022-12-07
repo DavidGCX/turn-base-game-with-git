1f0665d1ed27390af4adb40f8e8f9d7eeec9ce4a
DGCX <c62gao@uwaterloo.ca>
2022-12-07 12:50:08 -0500

    GridSystem

---
+++ Assets/Script/Gridsystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+    private int width;
+    private int height;
+    private int cellSize;
+    public GridSystem(int width, int height, int cellSize) {
+        this.cellSize = cellSize;
+        this.width = width;
+        this.height = height;
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right *0.3f, Color.green);
+            }
+        }
+    }
+
+    public Vector3 GetWorldPosition(int x, int z) {
+        return new Vector3(x, 0, z) * cellSize;
+    }
+}
