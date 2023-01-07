0095a60f8b9f816f39aeec481a542065b74617dd
DavidGCX <c62gao@uwaterloo.ca>
2023-01-06 22:05:36 -0500

    just rename UnitAttackHandler

--- Assets/Script/Action/UnitSpecialAttackHandler.cs
+++ Assets/Script/Action/UnitSpecialAttackHandler.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitAttackHandler : MonoBehaviour
+public class UnitSpecialAttackHandler : MonoBehaviour
 {
     [SerializeField] private Transform bulletPrefab;
     [SerializeField] private Transform Gunfront;
