51d3390c94b2833b1d242aa4c1447fc7d7ae2424
DavidGCX <c62gao@uwaterloo.ca>
2023-01-27 19:30:24 -0500

    Vague up

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -20,7 +20,7 @@ public class InputManager : MonoBehaviour
     //public bool GetMouseClick() => Mouse.current.leftButton.IsPressed(); this also work
     public bool GetMouseClick() => playerInput.Player.MouseClick.WasPressedThisFrame();
 
-     public bool GetMouseClickRight() => playerInput.Player.MouseClickRight.WasPressedThisFrame();
+    public bool GetMouseClickRight() => playerInput.Player.MouseClickRight.WasPressedThisFrame();
     public Vector2 GetCameraMove() => playerInput.Player.CameraMove.ReadValue<Vector2>();
     public float GetCameraRotate() => playerInput.Player.CameraRotate.ReadValue<float>();
 
