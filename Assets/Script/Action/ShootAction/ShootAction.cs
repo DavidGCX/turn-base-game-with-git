53a080893d0eb7a2ea5f3eb3a139e01d27c87700
DavidGCX <c62gao@uwaterloo.ca>
2023-01-27 19:59:50 -0500

    ScreenShake Instence Add

--- Assets/Script/Action/ShootAction/ShootAction.cs
+++ Assets/Script/Action/ShootAction/ShootAction.cs
@@ -12,9 +12,11 @@ public class ShootAction : AttackAction
     [SerializeField] private Transform gunFront;
 
     [SerializeField] private Transform gunBack;
+
     protected override IEnumerator SpecificAttack()
     {
         animator.Play("firing rifle");
+        ScreenShake.Instance.Shake();
         Transform bullet = Instantiate(bulletPrefab, gunFront.position, Quaternion.identity);
         bullet.GetComponent<Bullet>().SetUp(this, (targetUnit.GetWorldPosition() - unit.GetWorldPosition()).normalized, targetUnit.GetWorldPosition());
         yield return new WaitForSeconds(1f);
---
+++ Assets/Script/ScreenShake.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+
+public class ScreenShake : MonoBehaviour
+{
+    public static ScreenShake Instance {get; private set;}
+    CinemachineImpulseSource cinemachineImpulseSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    // Update is called once per frame
+    public void Shake(float intensity = 0.3f) {
+        cinemachineImpulseSource.GenerateImpulse(intensity);
+    }
+}
